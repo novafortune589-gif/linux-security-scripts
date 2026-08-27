e1c5d92eddca810e451e79aff7c8fd628bc97384
novafortune589-gif <novafortune589@gmail.com>
2026-08-27 01:11:42 +0100

    Add files via upload

---
+++ check.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+read -p " Hello sir. what the file name you want me to check if exist ? " file
+ while true; do
+       if [[ -f "$file" && -s "$file" ]] : then
+       
+              echo " the file $file exist "
+              echo " the systeme will shutdown in 1 minute !! "
+              sleep 60
+              sudo systemctl poweroff -i
+              break
+              
+       elif [ -f "$file" ];then 
+       
+              echo " file exist but empty "
+              echo " the systeme will shutdown in 1 minute !! "
+              sleep 60
+              sudo systemctl poweroff -i
+              break
+       else
+              echo " the file is not fond on the systeme yet... "
+              sleep 30
+       fi 
+done
